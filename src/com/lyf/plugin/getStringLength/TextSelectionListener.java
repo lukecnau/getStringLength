e57a42106ea368f37402406411bdcc8b31c35765
luke <luke.cn.au@gmail.com>
2014-12-02 00:49:27 +0800

    changed textselection to itextselection

--- src/com/lyf/plugin/getStringLength/TextSelectionListener.java
+++ src/com/lyf/plugin/getStringLength/TextSelectionListener.java
@@ -1,6 +1,6 @@
 package com.lyf.plugin.getStringLength;
 
-import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
@@ -20,8 +20,8 @@ public class TextSelectionListener implements ISelectionListener {
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
 		
-		if ( selection instanceof TextSelection ) {
-			TextSelection t = (TextSelection) selection;
+		if ( selection instanceof ITextSelection ) {
+			ITextSelection t = (ITextSelection) selection;
 			if ( this.callback != null )
 				callback.updateLength(t.getLength());
 		}
